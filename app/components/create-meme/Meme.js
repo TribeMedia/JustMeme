fddb3694a3d07c3ab8c3f8453d424c60dac6bc39
TJ VanToll <tj.vantoll@gmail.com>
2015-05-15 09:42:41 -0400

    Use lodash instead of a custom implementation

--- app/components/create-meme/Meme.js
+++ app/components/create-meme/Meme.js
@@ -3,12 +3,17 @@ var observable = require("data/observable");
 
 var imageManipulation = require("../image-manipulation/image-manipulation");
 var localStorage = require("../../shared/local-storage/local-storage");
-var socialShare = require("../../node_modules/nativescript-social-share/social-share");
 var utilities = require("../../shared/utilities");
 var analyticsMonitor = require("../../shared/analytics");
 
+var socialShare = require("../../node_modules/nativescript-social-share/social-share");
+var _ = require("../../node_modules/lodash/index");
+
 function Meme() {
 	var that = this;
+	var debouncedRefresh = _.debounce(function() {
+		that.refresh();
+	}, 50, { leading: true });
 
 	// Add an event listener to refresh the memeImage every time there is a change to the properties
 	this.addEventListener(observable.Observable.propertyChangeEvent, function(changes) {
@@ -18,9 +23,7 @@ function Meme() {
 		}
 
 		// Call refresh meme, but make sure it doesn't get called more often than every 200ms
-		callOncePerGivenTime(function() {
-			that.refresh();
-		}, 200);
+		debouncedRefresh();
 	});
 }
 Meme.prototype = new observable.Observable();
@@ -76,31 +79,5 @@ Meme.prototype.share = function() {
 	analyticsMonitor.trackFeature("CreateMeme.Share");
 	socialShare.shareImage(this.memeImage);
 };
-
-var shouldDelayNextCall = false;
-var additonalUpdateRequested = false;
-function callOncePerGivenTime(delegate, delay) {
-	//skip if an update has already been requested
-	if (shouldDelayNextCall) {
-		additonalUpdateRequested = true;
-		return;
-	}
-
-	shouldDelayNextCall = true;
-
-	// call the function here
-	delegate();
-
-	//delay the next call by a bit it, to make the app a bit more cost effective
-	setTimeout(function() {
-		shouldDelayNextCall = false;
-
-		//call the function again in case there was a request during the blocking period
-		if (additonalUpdateRequested) {
-			additonalUpdateRequested = false;
-			delegate();
-		}
-	}, delay);
-}
 
 module.exports = Meme;
